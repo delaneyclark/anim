392769faf69ab3f3109026097a47a67f56fefc81
delaney clark <delaneyclark6@gmail.com>
2015-05-06 13:51:24 -0400

    working more, transitions opacity

--- code.js
+++ code.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
 	$("#door").click(function(){ 
 		$("#doorway").toggleClass("show");
+		$("#bball").toggleClass("hide");
+		$("#win").toggleClass("hide")
 	})
 
 	$("#ball").click(function(){ 
+		$("#doorway").toggleClass("hide");
 		$("#bball").toggleClass("show");
+		$("#win").toggleClass("hide")
 	})
 
 	$("#window").click(function(){ 
-		$("#win").toggleClass("show");
+		$("#doorway").toggleClass("hide");
+		$("#bball").toggleClass("hide");
+		$("#win").toggleClass("show")
 	})
 })
